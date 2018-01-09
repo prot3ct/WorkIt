b12546f1aba07b4bb5fb5f50baf5a32627dc6a22
prot3ct <djorkaevhell@abv.bg>
2018-01-09 02:23:43 +0200

    Enable DB migrations, host the server on Azure and initialize db

--- WorkIt/app/src/main/java/prot3ct/workit/config/ApiConstants.java
+++ WorkIt/app/src/main/java/prot3ct/workit/config/ApiConstants.java
@@ -3,7 +3,7 @@ package prot3ct.workit.config;
 import prot3ct.workit.config.base.ApiConstantsContract;
 
 public class ApiConstants implements ApiConstantsContract {
-    private static final String API_URL = "localhost:8180/api";
+    private static final String API_URL = "https://workit.azurewebsites.net/api/jobs/api";
     private static final String URL_SIGN_IN = API_URL + "/auth/login";
     private static final String URL_SIGN_UP = API_URL + "/auth/register";
 
